e4688b3b1d6590691aa452187083b349a22ae6be
UserJavaUser <57154584+UserJavaUser@users.noreply.github.com>
2019-12-25 18:28:08 +0300

    Update Appliance.java

--- task01/entity/Appliance.java
+++ task01/entity/Appliance.java
@@ -1,5 +1,47 @@
 package by.tc.task01.entity;
 
 public class Appliance {
+  private double price;
+
+	public Appliance() {
+		
+	}
+	
+	public Appliance(double price) {
+		super();
+		this.price = price;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		long temp;
+		temp = Double.doubleToLongBits(price);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Appliance other = (Appliance) obj;
+		if (Double.doubleToLongBits(price) != Double.doubleToLongBits(other.price))
+			return false;
+		return true;
+	}
 
 }
